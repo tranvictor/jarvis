258d8eef3c6d5adefe46d03e31dba54d0fb52455
Victor Tran <vu.tran54@gmail.com>
2019-12-09 03:04:45 +0700

    add address lookup command

---
+++ cmd/address.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/tranvictor/jarvis/db"
+)
+
+// txCmd represents the tx command
+var addressCmd = &cobra.Command{
+	Use:   "addr",
+	Short: "find at max 10 matching addresses",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		para := strings.Join(args, " ")
+		addrs, scores := db.GetAddresses(para)
+		fmt.Printf("%12s  Addresses\n", "Scores")
+		fmt.Printf("-----------------------\n")
+		for i, addr := range addrs {
+			fmt.Printf("%12d  %s: %s\n", scores[i], addr.Address, addr.Desc)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addressCmd)
+}
--- db/address_database.go
+++ db/address_database.go
@@ -1,50 +1,68 @@
 package db
 
 import (
-  "strings"
-  "fmt"
+	"fmt"
+	"strings"
 
-  "github.com/tranvictor/ethutils/txanalyzer"
-  "github.com/sahilm/fuzzy"
+	"github.com/sahilm/fuzzy"
+	"github.com/tranvictor/ethutils/txanalyzer"
 )
 
+func getAddressMatches(input string, source FuzzySource) ([]AddressDesc, []int) {
+	matches := fuzzy.FindFrom(strings.Replace(input, " ", "_", -1), source)
+	result := []AddressDesc{}
+	scores := []int{}
+	for i := 0; i < 10; i++ {
+		if i < len(matches) {
+			result = append(result, source[matches[i].Index])
+			scores = append(scores, matches[i].Score)
+		} else {
+			break
+		}
+	}
+	return result, scores
+}
+
+func GetAddresses(input string) ([]AddressDesc, []int) {
+	source := NewFuzzySource()
+	return getAddressMatches(input, source)
+}
+
 func GetAddress(input string) (AddressDesc, error) {
-  source := NewFuzzySource()
-  matches := fuzzy.FindFrom(strings.Replace(input, " ", "_", -1), source)
-  if len(matches) == 0 {
-    return AddressDesc{}, fmt.Errorf("No address is found with '%s'", input)
-  }
-  match := matches[0]
-  return source[match.Index], nil
+	source := NewFuzzySource()
+	matches, _ := getAddressMatches(input, source)
+	if len(matches) == 0 {
+		return AddressDesc{}, fmt.Errorf("No address is found with '%s'", input)
+	}
+	return matches[0], nil
 }
 
 func GetTokenAddress(input string) (AddressDesc, error) {
-  source := NewTokenFuzzySource()
-  matches := fuzzy.FindFrom(strings.Replace(input, " ", "_", -1), source)
-  if len(matches) == 0 {
-    return AddressDesc{}, fmt.Errorf("No address is found with '%s'", input)
-  }
-  match := matches[0]
-  return source[match.Index], nil
+	source := NewTokenFuzzySource()
+	matches, _ := getAddressMatches(input, source)
+	if len(matches) == 0 {
+		return AddressDesc{}, fmt.Errorf("No address is found with '%s'", input)
+	}
+	return matches[0], nil
 }
 
 func AllTokenAddresses() map[string]string {
-  result := map[string]string{}
-  for addr, desc := range TOKENS {
-    result[strings.ToLower(addr)] = desc
-  }
-  return result
+	result := map[string]string{}
+	for addr, desc := range TOKENS {
+		result[strings.ToLower(addr)] = desc
+	}
+	return result
 }
 
 func AllAddresses() map[string]string {
-  addrs := txanalyzer.NewDefaultAddressDatabase().Data
-  tokenAddrs := AllTokenAddresses()
-  result := map[string]string{}
-  for addr, desc := range addrs {
-    result[strings.ToLower(addr.Hex())] = desc
-  }
-  for addr, desc := range tokenAddrs {
-    result[addr] = desc
-  }
-  return result
+	addrs := txanalyzer.NewDefaultAddressDatabase().Data
+	tokenAddrs := AllTokenAddresses()
+	result := map[string]string{}
+	for addr, desc := range addrs {
+		result[strings.ToLower(addr.Hex())] = desc
+	}
+	for addr, desc := range tokenAddrs {
+		result[addr] = desc
+	}
+	return result
 }
