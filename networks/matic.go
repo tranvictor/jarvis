09b8ecd526ef340a522410dc5250420d4f5a7341
vietthang207 <vietthang207@gmail.com>
2021-11-04 19:50:48 +0700

    Update matic.go

--- networks/matic.go
+++ networks/matic.go
@@ -53,7 +53,7 @@ func (self *matic) GetNodeVariableName() string {
 
 func (self *matic) GetDefaultNodes() map[string]string {
 	return map[string]string{
-		"infura":     "https://polygon-mainnet.infura.io/v3/683ba91d845e4bf2853828c85fd7145b",
+		"infura":     "https://polygon.kyber.network/v1/mainnet/hpc?appId=jarvis",
 		"maticvigil": "https://rpc-mainnet.maticvigil.com",
 	}
 }
